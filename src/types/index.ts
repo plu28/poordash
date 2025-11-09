e4d77a09af4f2078ce1f992069cf817bfe0690d6
David Voronov <davidvoronof@gmail.com>
2025-11-09 14:55:48 -0800

    added more to an order type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,30 @@
 export interface Review {
-  rating: number; // 1-5 stars
-  comment: string;
-  tags: string[];
-  date: string; // ISO date string
+	rating: number; // 1-5 stars
+	comment: string;
+	tags: string[];
+	date: string; // ISO date string
 }
 
 export interface Order {
-  id: string;
-  chefName: string;
-  dishName: string;
-  imageUrl: string;
-  orderDate: string; // ISO date string
-  review?: Review; // Optional, only if reviewed
+	id: string;
+	chefName?: string;
+	dishName: string;
+	imageUrl: string;
+	orderDate: string; // ISO date string
+	price: number;
+	options?: string[]; // Optional, only if specified
+	state: "NotStarted" | "InProgress" | "Ready" | "Complete"; // New order should be NotStarted 
+	deliveryAddress?: string; // Optional, only if this is a delivery order
+	review?: Review; // Optional, only if reviewed
 }
 
 export const PREDEFINED_TAGS = [
-  // Food quality
-  'tasty',
-  'fresh',
-  'well-portioned',
-  // Service
-  'fast delivery',
-  'friendly',
-  'accurate'
+	// Food quality
+	'tasty',
+	'fresh',
+	'well-portioned',
+	// Service
+	'fast delivery',
+	'friendly',
+	'accurate'
 ] as const;
